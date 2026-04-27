fb0d109e595fee5247bf503247d98be78a617aaf
Mike Michalczyk <memichalczyk2301@student.stcc.edu>
2026-04-27 13:17:37 -0400

    GitHub Setup/Player Class Change
    Set up GitHub Desktop
    
    Started Player class
    (Similar to other game)
    
    - Attributes
    - Constructors
    - Getter
    - Setter
    - Added a toString

--- core/src/main/java/io/github/TowerMan/Player.java
+++ core/src/main/java/io/github/TowerMan/Player.java
@@ -1,28 +1,94 @@
 package io.github.TowerMan;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
 public class Player extends Sprite {
     // Player-specific variables
-    private float velocityY = 0f;
+    private float velocityY;
     
     // Gravity and jump velocity can be adjusted as needed
     @SuppressWarnings("FieldMayBeFinal")
-    private float gravity = -15f;
+    private float gravity;
 
     @SuppressWarnings("FieldMayBeFinal")
-    private float jumpVelocity = 5f;
+    private float jumpVelocity;
 
     // Flag to check if the player is on the ground
-    private boolean isOnGround = true;
+    private boolean onGround;
 
     // 1 for right, -1 for left facing direction
     private int facingDirection = 1;
 
-    // Constructor to initialize the player with a texture and set initial position and size
-    public Player() {
-        setPosition(1, 0);
-        setSize(1, 1);
+    // Getters and setters for player variables
+    public float getVelocityY() {
+        return velocityY;
+    }
+
+    public void setVelocityY(float velocityY) {
+        this.velocityY = velocityY;
+    }
+
+    // Getters and setters for gravity
+    public float getGravity() {
+        return gravity;
+    }
+
+    public void setGravity(float gravity) {
+        this.gravity = gravity;
+    }
+
+    // Getters and setters for jump velocity
+    public float getJumpVelocity() {
+        return jumpVelocity;
+    }
+
+    public void setJumpVelocity(float jumpVelocity) {
+        this.jumpVelocity = jumpVelocity;
+    }
+
+    // Getters and setters for onGround flag
+    public boolean isOnGround() {
+        return onGround;
     }
     
+    public void setOnGround(boolean onGround) {
+        this.onGround = onGround;
+    }
+
+    // Getters and setters for facing direction
+    public int getFacingDirection() {
+        return facingDirection;
+    }
+
+    public void setFacingDirection(int facingDirection) {
+        this.facingDirection = facingDirection;
+    }
+
+    // Constructor to initialize the player with a texture and set initial position and size
+    public Player(Texture texture) {
+        // Call the Sprite constructor to initialize the sprite with the given texture
+        super(texture);
+        
+        // Set initial position and size of the player
+        setPosition(1, 0);
+        setSize(1, 1);
+
+        // Initialize player-specific variables
+        velocityY = 0f;
+        gravity = -15f;
+        jumpVelocity = 5f;
+        onGround = true;
+    }
+    
+    @Override
+    public String toString() {
+        return "Player{" +
+                "velocityY=" + velocityY +
+                ", gravity=" + gravity +
+                ", jumpVelocity=" + jumpVelocity +
+                ", onGround=" + onGround +
+                ", facingDirection=" + facingDirection +
+                '}';
+    }
 }
